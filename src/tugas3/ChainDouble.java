2cf6d2ec976a800366a077b7780ba8cacb23c813
Fiki L <Fiki L@DESKTOP-5OC1DOK>
2017-10-10 23:28:28 +0700

    Tugas3 Nanda Ageng 201610370311186

--- src/tugas3/ChainDouble.java
+++ src/tugas3/ChainDouble.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tugas3;
 
-/**
- *
- * @author Fiki L
- */
 public class ChainDouble implements LinearList{
     
     protected ChainNodeDouble firstNode;
